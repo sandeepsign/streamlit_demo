ec45fbb776c88ea5143f9e3cc79669215c706fad
sandeepsign <sandeep.sign@gmail.com>
2024-03-05 14:48:53 -0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+side_bar = st.sidebar.selectbox('Selector Name', ('Choice 1','Choice 2'))
+
+if side_bar == 'Choice 1':
+     st.write('Choice 1')
+
+if side_bar == 'Choice 2':
+     st.write('Choice 2')
+
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.header('st.write')
+
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
